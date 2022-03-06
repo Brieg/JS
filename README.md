53a27b73f2cd1e320bd39ed209705897c96e1e62
Mariusz Briegmann <mariuszbrieg@gmail.com>
2022-03-06 19:54:58 +0100

    Gather Years from API

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 #Car Vibes
 
-JS
+Avoid CORS
+
+Visual Studio code
+1. Press Ctrl + Shift + X
+2. Paste "Live Server" and install (^v5.7.4)
+3. Open "index.html" in VS
+4. Press "Go Live" on right bottom window 
+
+Download this plugin for Chrome
 https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf
+
+1. Press big icon "C:" on the plugin 
+2. Refresh CarVibes page
+3. Profit.
--- js/global.js
+++ js/global.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     const ManufacturerSelect = document.getElementById("car-brand-one");
     const ModelSelect = document.getElementById("car-model-one");
+    const YearSelect = document.getElementById("car-year-one");
     const ManufacturerSecondSelect = document.getElementById("car-brand-second");
     const ModelSecondSelect = document.getElementById("car-model-second");
+    const YearSecondSelect = document.getElementById("car-year-second");
 
-    function getManufacturer(selectElement) {
+    getManufacturer = (selectElement) => {
         
         fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json')
         .then(response => response.json())
@@ -20,10 +22,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         });
     }
 
-    getManufacturer(ManufacturerSelect);
-    getManufacturer(ManufacturerSecondSelect);
-
-    function setSelectModelByManufacturerID(selectNodeElement, ManufacturerID) {
+    setSelectModelByManufacturerID = (selectNodeElement, ManufacturerID) => {
         if(!isNaN(ManufacturerID)) {
             fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/'+ManufacturerID+'?format=json')
             .then(response => response.json())
@@ -41,12 +40,44 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     }
 
+    range = (start, end) => [...Array(end - start + 1)].map((_, i) => start + i);
 
-    ManufacturerSelect.addEventListener("change", function() {
+    setAvailableYears = (selectElement) => {
+        fetch('https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getYears')
+        .then(response => response.text())
+        .then(text => {
+                //text: ({ "Years": {"min_year":"1941", "max_year":"2022"} });
+                let data = text.substring(2, text.length -2); // remove first two leters "?(" character and last ");" from API reponse
+                data = JSON.parse(data);
+
+                selectElement.innerHTML = null;
+                selectElement.removeAttribute("disabled");
+
+                let years = range(parseInt(data.Years.min_year), parseInt(data.Years.max_year));
+
+                for ( let i = 0; i < years.length; i++) {
+                    selectElement.options[i] = new Option( years[i],  years[i]);
+                }
+            }
+        )  
+        .catch((error) => {
+            console.error('Error:', error);
+        }); 
+    }
+
+
+    getManufacturer(ManufacturerSelect);
+    getManufacturer(ManufacturerSecondSelect);
+
+    setAvailableYears(YearSelect);
+    setAvailableYears(YearSecondSelect);
+
+
+    YearSelect.addEventListener("change", function() {
         setSelectModelByManufacturerID(ModelSelect, this.selectedOptions[0].value);
     });
 
-    ManufacturerSecondSelect.addEventListener("change", function() {
+    YearSecondSelect.addEventListener("change", function() {
         setSelectModelByManufacturerID(ModelSecondSelect, this.selectedOptions[0].value);
     });
 
